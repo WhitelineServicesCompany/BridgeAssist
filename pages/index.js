8c6aeea7e2450e14224afc6a9f2bd1a88a348841
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-09-29 14:04:15 +0100

    optimisation et restructuration du code de la page d'acceuil

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,69 @@ import Bouton from '@/components/Button/Button'
 import Footer from '@/components/Footer/Footer';
 import Link from 'next/link';
 export default function Home() {
+  const Benefits = () => {
+    const benefitsData = [
+      {
+        image: "/images/Innovation et expertise-11.png",
+        title: "Innovation et expertise",
+        description: "Chez Bridge Assist, nous combinons notre profonde connaissance du secteur des assurances avec une approche innovante pour offrir des solutions d'assistance automobile, domiciliaire, bris de glace et voyage efficaces et adaptées aux besoins de nos clients."
+      },
+      {
+        image: "/images/Service Complet-11.png",
+        title: "Service complet 24/7",
+        description: "notre dévouement envers nos clients ne s'arrête jamais. Nous sommes disponibles 24 heures sur 24, 7 jours sur 7, pour une assistance rapide et efficace à tout moment. Que ce soit pour une panne de voiture, un problème à domicile, lors d’un voyage ou pour bris de glace, nos équipes chevronnées sont à votre disposition."
+      },
+      {
+        image: "/images/Proximité-11.png",
+        title: "Proximité avec vous",
+        description: "chez Bridge Assist, nous construisons des relations fortes et durables. Nous valorisons la proximité et nous nous engageons à être le partenaire de confiance sur lequel vous pouvez compter pour tous vos besoins d'assistance. Nous sommes plus qu'une simple compagnie d'assistance, nous sommes votre allié en toutes circonstances."
+      }
+    ];
+
+    return (
+      <div className="row m-0 m-auto width2 mt-5">
+        {benefitsData.map((benefit, index) => (
+          <div key={index} className="col-xl-4 col-lg-6 col-md-6 mx-auto col-sm-12 mb-5">
+            <BenefitBox data={benefit} />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const BenefitBox = ({ data }) => (
+    <div className={`${style.rounded1} ${style.gradientbox} ${style.contentbox}`}>
+      <Image src={data.image} alt={data.title} width={50} height={50} />
+      <h2>{data.title}</h2>
+      <p>{data.description}</p>
+    </div>
+  );
+  const productsData = [
+    {
+      href: "/Produits/B-Auto",
+      image: "/images/BA_Website_AUTO-10.png",
+      title: "B-AUTO",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam"
+    },
+    {
+      href: "/Produits/B-Travel",
+      image: "/images/BA_Website_B-Travel-10.png",
+      title: "B-TRAVEL",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam"
+    },
+    {
+      href: "/Produits/B-Glass",
+      image: "/images/glassFiltre.png",
+      title: "B-GLASS",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam"
+    },
+    {
+      href: "/Produits/B-Home",
+      image: "/images/BA_Website_PagesHOME PAGE-10.png",
+      title: "B-HOME",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam"
+    }
+  ];
   return (
     <>
       <Head>
@@ -16,138 +79,36 @@ export default function Home() {
       </Head>
       <main>
         <Header imgHeader={'BA_Website_PagesHOME PAGE-10.png'} title='Un pont vers une vie épanouie chez vous' />
-        <div className={`text-center m-auto mt-5 width2`} style={{ color: '#004C97' }}>
+        <div className={`text-center m-auto mt-5 width2 blueColor`}>
           <h1>Pourquoi nous choisir ?</h1>
           <p className='m-3'>Choisir Bridge Assist, c'est opter pour une tranquillité d'esprit inégalée. Nous offrons une assistance personnalisée, réactive et innovante. Découvrez
             pourquoi nous sommes le choix idéal :</p>
         </div>
-        <div className="row m-0 m-auto width2 mt-5">
-          <div className="col-lg-4 col-md-4 col-sm-12 mb-5" >
-            <div className={`${style.rounded1} ${style.gradientbox} ${style.contentbox}`}>
-              <Image
-                src="/images/Innovation et expertise-11.png"
-                alt="Innovation et expertise"
-                width={50}
-                height={50}
-              />
-              <h2>Innovation et expertise</h2>
-              <p>
-                Chez Bridge Assist, nous combinons notre profonde connaissance du secteur des assurances
-                avec une approche innovante pour offrir des solutions d'assistance automobile, domiciliaire,
-                bris de glace et voyage efficaces et adaptées aux besoins de nos clients.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-4 col-sm-12 mb-5">
-            <div className={`${style.rounded1} ${style.gradientbox} ${style.contentbox}`}>
-              <Image
-                src="/images/Service Complet-11.png"
-                alt="Innovation et expertise"
-                width={50}
-                height={50}
-              />
-              <h2>Service complet 24/7</h2>
-              <p>
-                notre dévouement envers nos
-                clients ne s'arrête jamais. Nous
-                sommes disponibles 24 heures
-
-                sur 24, 7 jours sur 7, pour une as-
-                sistance rapide et ecace à tout
-
-                moment. Que ce soit pour une
-                panne de voiture, un problème
-                à domicile, lors d’un voyage ou
-                pour bris de glace, nos équipes
-
-                chevronnées sont à votre dispo-
-                sition.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-4 col-sm-12 mb-5">
-            <div className={`${style.rounded1} ${style.gradientbox} ${style.contentbox}`}>
-              <Image
-                src="/images/Proximité-11.png"
-                alt="Innovation et expertise"
-                width={50}
-                height={50}
-              />
-              <h2>Proximité avec vous</h2>
-              <p>
-                chez Bridge Assist, nous construi-
-                sons des relations fortes et du-
-                rables. Nous valorisons la proxi-
-                mité et nous nous engageons à
-
-                être le partenaire de conance
-                sur lequel vous pouvez compter
-
-                pour tous vos besoins d'assistan-
-                ce. Nous sommes plus qu'une
-
-                simple compagnie d'assistance,
-                nous sommes votre allié en
-                toutes circonstances.
-              </p>
-            </div>
-          </div>
-        </div>
+        <Benefits />
         <h1 className="text-center fs-4 blueColor">Explorez Bridge Assist en téléchargeant notre présentation</h1>
         <Bouton />
 
-        <div style={{ position: 'relative' }}>
-          <div style={{ position: 'absolute', height: '80%', width: '100%', top: '0', zIndex: '0', backgroundColor: '#004C97' }}>
+        <div className={`${style.parentdiv}`}>
+          <div className={`${style.firstchilddiv}`} >
 
           </div>
-          <div className='width2 mx-auto py-5' style={{ zIndex: '99', position: 'relative' }}>
-            <h1 className='text-center mb-5 ' style={{ color: 'white', zIndex: '99' }}>Nos produits</h1>
-            <div className={`${style.conteneur}`} style={{ width: '100%', zIndex: '99' }}>
-              <Link href="/Produits/B-Auto" legacyBehavior style={{width:'50%'}}>
-              <div style={{cursor: 'pointer'}} className={`${style.contimg} ${style.img1}`}>
-                    <img src="/images/BA_Website_AUTO-10.png" alt="Image 1" className={`${style.img1}`} />
+          <div className={`${style.secondchilddiv} width2 mx-auto py-5`}>
+            <h1 className={`${style.headertext}`}>Nos produits</h1>
+            <div className={`${style.conteneur}`}>
+              {productsData.map((product, index) => (
+                <Link key={index} href={product.href} legacyBehavior>
+                  <div className={`${style.contimg} ${style[`img${index + 1}`]}`}>
+                    <img src={product.image} alt={`Image ${index + 1}`} className={`${style[`img${index + 1}`]}`} />
                     <div className={`${style.Hovcontimg} text-white`}>
-                      <h1 className='mb-4'>B-AUTO</h1>
-                      <p className={`${style.para} width2 text-center`}>Lorem ipsum dolor sit amet, consectetuer adipiscing
-                        elit, sed diam nonummy nibh euismod tincidunt ut
-                        laoreet dolore magna aliquam</p>
+                      <h1 className='mb-4'>{product.title}</h1>
+                      <p className={`${style.para} width2 text-center`}>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing {product.description}
+                      </p>
                     </div>
                   </div>
-              </Link>
-              <Link href="/Produits/B-Travel" legacyBehavior style={{width:'50%'}}>
-    <div style={{cursor: 'pointer'}}  className={`${style.contimg} ${style.img2}`}>
-                <img src="/images/BA_Website_B-Travel-10.png" alt="Image 2" className={`${style.img2}`} />
-                <div className={`${style.Hovcontimg} text-white`}>
-                  <h1 className='mb-4'>B-TRAVEL</h1>
-                  <p className={`${style.para} width2 text-center`}>Lorem ipsum dolor sit amet, consectetuer adipiscing
-                    elit, sed diam nonummy nibh euismod tincidunt ut
-                    laoreet dolore magna aliquam</p>
-                </div>
-              </div>
-              </Link>
-              <br className={`${style.br}`} />
-              <Link href="/Produits/B-Glass" legacyBehavior style={{width:'50%'}}>
-              <div style={{cursor: 'pointer'}}  className={`${style.contimg} ${style.img3}`}>
-                <img src="/images/glassFiltre.png" alt="Image 3" className={`${style.img3}`} />
-                <div className={`${style.Hovcontimg} text-white`}>
-                  <h1 className='mb-4'>B-GLASS</h1>
-                  <p className={`${style.para} width2 text-center`}>Lorem ipsum dolor sit amet, consectetuer adipiscing
-                    elit, sed diam nonummy nibh euismod tincidunt ut
-                    laoreet dolore magna aliquam</p>
-                </div>
-              </div>
-              </Link>
-              <Link href="/Produits/B-Home" legacyBehavior style={{width:'50%'}}>
-              <div style={{cursor: 'pointer'}}  className={`${style.contimg} ${style.img4}`}>
-                <img src="/images/BA_Website_PagesHOME PAGE-10.png" alt="Image 4" className={`${style.img4}`} />
-                <div className={`${style.Hovcontimg} text-white`}>
-                  <h1 className='mb-4'>B-HOME</h1>
-                  <p className={`${style.para} width2 text-center`}>Lorem ipsum dolor sit amet, consectetuer adipiscing
-                    elit, sed diam nonummy nibh euismod tincidunt ut
-                    laoreet dolore magna aliquam</p>
-                </div>
-              </div>
-              </Link>
+                </Link>
+              ))}
+
             </div></div>
 
         </div>
